8a63352cfdbede4e12223cfed643e0f979b88df4
prevostc <998369+prevostc@users.noreply.github.com>
2025-03-20 12:28:00 +0100

    Also create token

--- src/mapping/token-balance.ts
+++ src/mapping/token-balance.ts
@@ -4,6 +4,7 @@ import { Transfer } from "../../generated/templates/TokenBalance/IERC20"
 import { ADDRESS_ZERO } from "../utils/address"
 import { SHARE_TOKEN_MINT_ADDRESS } from "../config"
 import { getInvestor } from "../entity/investor"
+import { getToken } from "../entity/token"
 
 export function handleTokenBalanceTransfer(event: Transfer): void {
   const token = event.address
@@ -12,11 +13,14 @@ export function handleTokenBalanceTransfer(event: Transfer): void {
   const value = event.params.value
 
   if (from.equals(to)) {
-    log.debug("handleTokenBalanceTransfer: from and to are the same address, ignoring", [])
+    // log.debug("handleTokenBalanceTransfer: token {}, trx {}, from and to are the same address, ignoring", [
+    //   token.toHexString(),
+    //   event.transaction.hash.toHexString(),
+    // ])
     return
   }
   if (value.isZero()) {
-    log.debug("handleTokenBalanceTransfer: value is zero, ignoring", [])
+    //log.debug("handleTokenBalanceTransfer: token {}, trx {}, value is zero, ignoring", [token.toHexString(), event.transaction.hash.toHexString()])
     return
   }
 
@@ -31,6 +35,9 @@ export function handleTokenBalanceTransfer(event: Transfer): void {
 function updateTokenBalance(tokenAddress: Bytes, investorAddress: Bytes, diff: BigInt): void {
   const investor = getInvestor(investorAddress)
   investor.save()
+
+  const token = getToken(tokenAddress)
+  token.save()
 
   const tokenBalance = getTokenBalance(investorAddress, tokenAddress)
   tokenBalance.rawBalance = tokenBalance.rawBalance.plus(diff)
